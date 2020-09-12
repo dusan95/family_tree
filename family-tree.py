0caa8618e6bd878b647a38ba7adefc2fdb262797
Dusan Miljkovic <dusanmiljkovic_95@hotmail.com>
2020-09-12 19:48:22 +0200

    Code refactoring

--- family-tree.py
+++ family-tree.py
@@ -2,64 +2,131 @@ from os.path import join, dirname, isfile
 from textx import metamodel_from_file
 from textx.export import metamodel_export, model_export
 
-def main(fileName, personId, debug=False):
-    # Get meta-model from language description and export it to dot   GRAMATIKA
+class Person(object):
+    def __init__(self, id = "", name = "", lastName = "", dateOfBirth = "", alive = "", gender = "",
+     parent1 = "", parent2 = "", supose = "" ):
+        self.id = id
+        self.name = name
+        self.lastName = lastName
+        self.dateOfBirth = dateOfBirth
+        self.alive = alive
+        self.gender = gender
+        self.parent1 = parent1
+        self.parent2 = parent2
+        self.supose = supose
+
+
+this_folder = dirname(__file__)
+do = True
+fileName = ""
+
+def getFamilyTreeModel(debug = False):
+    """
+    A function that gets meta-model from language description and export it to dot
+    and instantiate model and export it to dot       
+    """
     family_tree_meta = metamodel_from_file(join(this_folder, 'family-tree.tx'), debug=debug)
     metamodel_export(family_tree_meta, join(this_folder, 'family-tree_meta.dot'))
-
-    # Instantiate model and export it to dot      FAJL KOJI SE UCITAVA
     family_tree_model = family_tree_meta.model_from_file(join(this_folder, fileName))
     model_export(family_tree_model, join(this_folder, 'family-tree.dot'))
+    return family_tree_model
 
-    for person in family_tree_model.persons:
-        if person.name == personId:
-            print("{} {}'s parents are {} {} and {} {}".format(person.firstName, person.lastName,
-            person.parent1.firstName, person.parent1.lastName, person.parent2.firstName, person.parent2.lastName))
+def bornOn(person):
+    """
+    A function that prints out persons date of birth.
+    """
+    if person.dateOfBirth is not None :
+        print("\n{} {} date of birth {}.{}.{}.".format(person.firstName, person.lastName, person.dateOfBirth.day, person.dateOfBirth.month, person.dateOfBirth.year))
+    else:
+        print("{} {} unknown date of birth.".format(person.firstName, person.lastName))
 
-    #for command in family_tree_model.commands:
-     #   if command.__class__.__name__ == "QueryParentCommand":
-      #      print("{} is parent of {}".format(command.p.firstName, command.c.firstName))
-       #     #ako je klasa parent komanda treba napraviti vezu
-        #if command.__class__.__name__ == "GenderCommand":
-         #   print("{} {} is {}".format(command.person.firstName, command.person.lastName, command.gender))
-        #if command.__class__.__name__ == "StatusCommand":
-         #   print("{} {} is {}".format(command.person.firstName, command.person.lastName, command.status))
+def showSpouses(person):
+    """
+    A function that prints out persons spouse or spouses.
+    """
+    if person.spouses is not None :
+        for sp in person.spouses:
+            if sp.since is not None:
+                print("Spouse {} {}, since {}.{}.{}.".format(sp.person.firstName, sp.person.lastName, sp.since.day, sp.since.month, sp.since.year))
+            else:
+                print("Spouse {} {}".format(sp.person.firstName, sp.person.lastName))
 
-def displayPersonData(fileName, personId,  debug = False):
-    # Get meta-model from language description and export it to dot   GRAMATIKA
-    family_tree_meta = metamodel_from_file(join(this_folder, 'family-tree.tx'), debug=debug)
-    metamodel_export(family_tree_meta, join(this_folder, 'family-tree_meta.dot'))
+def showParents(person):
+    """
+    A function that prints out persons parents.
+    """
+    if person.parent1 is not None and person.parent2 is not None:
+        print("Parents: {} {} and {} {}".format(person.parent1.firstName, person.parent1.lastName, person.parent2.firstName, person.parent2.lastName))
+    else:
+        if person.parent1 is not None:
+            print("Parent: {} {}".format(person.parent1.firstName, person.parent1.lastName))
+        if person.parent2 is not None:
+            print("Parent: {} {}".format(person.parent2.firstName, person.parent2.lastName))
 
-    # Instantiate model and export it to dot      FAJL KOJI SE UCITAVA
-    family_tree_model = family_tree_meta.model_from_file(join(this_folder, fileName))
-    model_export(family_tree_model, join(this_folder, 'family-tree.dot'))
+def showBrothersAndSisters(person):
+    """
+    A function that prints out brothers and sisters of a person.
+    """
+    brothersAndSisters = []
+    if person.parent1 is not None:
+        for c in person.parent1.children:
+            if not c.person.name == person.name:
+                brothersAndSisters.append(Person(c.person.name, c.person.firstName, c.person.lastName))
+        exists = False
+    if person.parent2 is not None:   
+        for c in person.parent2.children:
+            if not c.person.name == person.name:
+                for bns in brothersAndSisters:
+                    if bns.id == c.person.name:
+                        exists = True
+                if not exists:
+                    brothersAndSisters.append(Person(c.person.name, c.person.firstName, c.person.lastName))
+    if brothersAndSisters.count != 0:
+        print("Brothers and sisters:")
+        for bns in brothersAndSisters:
+            print("\t{} {}".format(bns.name, bns.lastName))
+
+def displayPersonData(personId,  debug = False):
+    family_tree_model = getFamilyTreeModel()
     found = False
     for person in family_tree_model.persons:
         if person.name == personId:
-            if person.dateOfBirth is not None :
-                print("{} {} is born on {}.{}.{}.".format(person.firstName, person.lastName, person.dateOfBirth.day, person.dateOfBirth.month, person.dateOfBirth.year))
-            else:
-                print("{} {}.".format(person.firstName, person.lastName))
-            if person.spouses is not None :
-                for sp in person.spouses:
-                    print("Spouse {} {}".format(sp.spouse.firstName, sp.spouse.lastName))
+            bornOn(person)
+            showSpouses(person)
+            showParents(person)
+            showBrothersAndSisters(person)
             found = True
     if not found:
         print("The peson {} doesn't exist!".format(personId))
 
-def chooseRelationship(fileName):
+def chooseRelationship():
     print("Choose relationship:")
     print("1 - parents")
+    print("2 - brothers and sisters")
+    print("3 - grandparents")
+    print("4 - aunts")
+    print("5 - uncles")
+    print("6 - sisters in law")
+    print("7 - brother in law")
     choise = input()
-    if(choise == "1"):
-        print("Unesite osobu")
-        personId = input()
-        main(fileName, personId)
+    print("Enter person")
+    personId = input()
+    if choise == "1":
+        showParents(personId)
+    if choise == "2":
+        showBrothersAndSisters(personId)
+    if choise == "3":
+        showParents(personId)
+    if choise == "4":
+        showParents(personId)
+    if choise == "5":
+        showParents(personId)
+    if choise == "6":
+        showParents(personId)
+    if choise == "7":
+        showParents(personId)
 
 if __name__ == '__main__':
-    this_folder = dirname(__file__)
-    do = True
-    fileName = ""
     while do:
         print("Please enter filename")
         fileName = input()
@@ -67,24 +134,25 @@ if __name__ == '__main__':
             #proveriti da model li odgovara gramatici
             do = False
         else:
-            print("wrong")
+            print("\n\t\tWrong filename\n")
     do = True
     while do:
-        print("Choose one of the options: ")
+        print("\nChoose one of the options: ")
         print("1 - show person data")
-        print("2 - query ")
-        print("3 - end")
+        print("2 - show perons relationships")
+        print("3 - show relationship")
+        print("4 - end")
         choise = input()
         if(choise == "1"):
             print("Enter person name") 
             personId = input()
-            displayPersonData(fileName, personId)
+            displayPersonData(personId)
+        if(choise == "2"):
+            chooseRelationship()
+        if choise == "3":
+            continue
+        if(choise == "4"):
+            do = False
         else:
-            if(choise == "2"):
-                chooseRelationship(fileName)
-            else:
-                if(choise == "3"):
-                    do = False
-                else:
-                    print("wrong entry")
+            print("wrong entry")
 
